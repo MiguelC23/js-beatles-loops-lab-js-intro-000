febcdc1908925a614308531838e95b9732235531
M C <miguelcarmona996@gmail.com>
2019-08-16 16:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
-  }
-  return array;
-}
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
 
+//alters the array when called
+function johnLennonFacts(facts) {
+  var i = 0;
+    while (i < facts.length) {
+       facts[i] += "!!!";
+     i++;
+    }
+    console.log(facts);
+  }
+
+ //calls the function to alter the array
+ johnLennonFacts(facts)
+ 
 function iLoveTheBeatles(number) {
   var array = [];
   do {
@@ -26,4 +31,4 @@ function iLoveTheBeatles(number) {
     number += 1
   } while (number < 15);
   return array;
-} 
+}
