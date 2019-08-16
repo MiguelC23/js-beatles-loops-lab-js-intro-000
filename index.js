8ec7d020dfb7e8f9f7f58cec8b3409e8607ed8ad
M C <miguelcarmona996@gmail.com>
2019-08-16 16:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-'use strict';
+const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
+const instruments = ["guitar", "drums", "piano", "trumpet"];
 
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return array;
+
+var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
+	var collect = [];
+	for (var i = 0; i<musiciansArray.length; i++) {
+	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
+	}
+	return(collect);
+};
+
+theBeatlesPlay(singers, instruments);
+
+var facts = [
+	"He was the last Beatle to learn to drive",
+	"He was never a vegetarian",
+	"He was a choir boy and boy scout",
+	"He hated the sound of his own voice"
+];
+
+var johnLennonFacts = function (factsArray) {
+	var i = 0;
+	var arrayCounter = factsArray.length
+	var newFactsArray = [];
+
+	while (arrayCounter > 0) {
+		newFactsArray.push(factsArray[i] + "!!!");
+		i++;
+		arrayCounter--;
+	}
+	return(newFactsArray);
+};
+
+johnLennonFacts(facts);
+
+var iLoveTheBeatles = function (num){
+	var arr = [];
+	num++;
+
+	if (num === 18) {
+		return("I love the Beatles!");
+	}
+
+	var i = 0;
+	do {
+		arr.push("I love the Beatles!");
+		i++;
+
+	} while (i < num);
+
+	return(arr);
 }
 
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
-"He hated the sound of his own voice"];
-
-//alters the array when called
-function johnLennonFacts(facts) {
-  var i = 0;
-    while (i < facts.length) {
-       facts[i] += "!!!";
-     i++;
-    }
-    console.log(facts);
-  }
-
- //calls the function to alter the array
- johnLennonFacts(facts)
- 
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number += 1
-  } while (number < 15);
-  return array;
-}
+iLoveTheBeatles(2);
